31861d9a84f4eedf40d73dd6f7553b5d00330e5f
Mike-Like11 <57439449+Mike-Like11@users.noreply.github.com>
2021-10-03 15:51:52 +0300

    Стартовая страница

--- app/src/main/java/com/example/millioner/MainActivity.kt
+++ app/src/main/java/com/example/millioner/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.millioner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().replace(R.id.fragment_content, MenuStart()).commit()
     }
 }
